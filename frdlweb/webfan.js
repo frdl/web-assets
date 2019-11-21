dda4ee6b21c472696d2dc481781327c407511362
Till Wehowski <github@webfan.de>
2019-11-21 12:41:10 +0100

    Update webfan.js

--- frdlweb/webfan.js
+++ frdlweb/webfan.js
@@ -17,7 +17,7 @@ exports = module.exports = function(config){
 	var route_module_assets = project.ce_baseUrl + frdl.str_replace('[modules|themes:type]', 'modules', route);
 	var route_theme_assets = project.ce_baseUrl + frdl.str_replace('[modules|themes:type]', 'themes', route);
 	
-	config.hps.scriptengine.requirejs.paths['module-assets'] =  frdl.str_replace('//', '/',route_module_assets);
-	config.hps.scriptengine.requirejs.paths['theme-assets'] = frdl.str_replace('//', '/',route_theme_assets);
+	config.hps.scriptengine.requirejs.paths['module-assets'] =  frdl.str_replace('https:/', 'https://', frdl.str_replace('//', '/',route_module_assets));
+	config.hps.scriptengine.requirejs.paths['theme-assets'] =   frdl.str_replace('https:/', 'https://', frdl.str_replace('//', '/',route_theme_assets));
 	
 };
